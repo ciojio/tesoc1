b1ef9e338849ecac05bc221ec8971d9121306411
Wai Ko <waiphyo.ko@kbzbank.com>
2020-04-13 16:12:58 +0630

    host to ip

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname+'/index.html'));
     //res.send('Hello ? my world');
 });
-const PORT = 8080;
+const PORTS = 8080;
 // const HOST = '0.0.0.0';
 const HOST = 'localhost';
-const IP = '0.0.0.0';
+const IPA = '0.0.0.0';
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 // const HOST = 'test.com';
 
 // const port=process.env.PORT || 80;
@@ -23,5 +27,5 @@ const IP = '0.0.0.0';
 
 
 //app.listen(PORT,HOST);
-app.listen(PORT,IP);
+app.listen(port,ip);
 //console.log(`Running on http://${HOST}:${PORT}`);
